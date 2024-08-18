fd5e9872d141d5b9634af7c6d0373f9051d73452
efe şengül <oyundny01@gmail.com>
2024-08-18 17:31:53 +0300

    Shopping cart migration updated.

--- database/migrations/2024_08_17_160056_create_shopping_cart_table.php
+++ database/migrations/2024_08_17_160056_create_shopping_cart_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('shopping_cart', function (Blueprint $table) {
+        Schema::create('shopping_carts', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->unsignedBigInteger("user_id");
